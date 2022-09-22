69e92e3ac00d1b034a4cc32298164d6ee90cc356
Aland Mariwan <dev@aland-mariwan.de>
2022-09-22 10:42:05 +0000

    b

--- backend/index.js
+++ backend/index.js
@@ -6,42 +6,51 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
-var address = require('address');
+const PORT = 3001;
 
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app middleware
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+const SESSION_SECRET = process.env.SESSION_SECRET;
 
 app.use(
-	session({
-		secret: 'secret',
-		resave: true,
-		saveUninitialized: true
-	})
-);
+    session({
+        //store: "",
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: false,
+            httpOnly: false,
+            maxAge: 1000 * 60 * 10,
+        },
+    }));
 
 /* This is a middleware that is used to parse the body of the request. */
 app.use(
-	cors({
-		origin: [ 'http://localhost:3001' ],
-		methods: [ 'GET', 'POST' ],
-		credentials: true
-	})
+    cors({
+        origin: ['http://localhost:3001'],
+        methods: ['GET', 'POST'],
+        credentials: true
+    })
 );
 app.use(cookieParser());
-app.use(express.json());
+
 app.use(express.static('static'));
-app.use(express.urlencoded({ extended: true }));
 
 // Routers
 const authRouter = require('./routes/auth');
 app.use('/auth', authRouter);
 
 address(function(err, addrs) {
-	console.log(addrs.ip, addrs.ipv6, addrs.mac);
-	// '192.168.0.2', 'fe80::7aca:39ff:feb0:e67d', '78:ca:39:b0:e6:7d'
+    console.log(addrs.ip, addrs.ipv6, addrs.mac);
+    // '192.168.0.2', 'fe80::7aca:39ff:feb0:e67d', '78:ca:39:b0:e6:7d'
 });
 
 /* This is telling the server to listen to port 3001. */
-app.listen(3001, () => {
-	console.log('running in the 3001');
+app.listen(PORT, () => {
+    console.log('running in the ${PORT}');
 });
