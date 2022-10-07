a608c36d4592edcf8317a03895b9a9d96239ee78
Aland Mariwan <dev@aland-mariwan.de>
2022-10-07 19:33:27 +0200

    fixed clearAllCookie

--- backend/module/clearAllcookie.js
+++ backend/module/clearAllcookie.js
@@ -1,11 +1,12 @@
 const clearAllcookie = (req, res) => {
 	if (req == null || req.cookies == null || req.cookies.length == 0) return false;
-	const cookies = req.cookies;
-	console.log(cookies);
-	cookies.forEach((prop) => {
-
-		res.clearCookie(prop); //Or res.cookie(prop, '', {expires: new Date(0)});
-	});
+	cookie = req.cookies;
+	for (var prop in cookie) {
+			if (!cookie.hasOwnProperty(prop)) {
+					continue;
+			}    
+			res.cookie(prop, '', {expires: new Date(0)});
+	}
 	return true;
 };
 
